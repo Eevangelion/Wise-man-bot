4a6168b2f412c31502948ff9da50ca8aa1f6842d
Sakagam1 <platonov.ivan347@yandex.ru>
2024-08-03 13:03:32 +0300

    Remove fastapi, google drive interactions, add simple get content from file id

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-import bot
-from fastapi import FastAPI
+import os, json, secrets
+import telebot
 
-app = FastAPI()
+import utils
 
-@app.get("/")
-def run():
-    bot.Run()
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv("TG_TOKEN")
+chat_id = int(os.getenv("CHAT_ID"))
+user_id = os.getenv("USER_ID")
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['get_quote'])
+def get_quote(message):
+    quotes = utils.get_quotes()
+    num = secrets.SystemRandom().randint(0, len(quotes) - 1)
+    quote = quotes[num]
+    for ch in ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!']:
+        quote = ("\\" + ch).join(quote.split(ch))
+    quote = ("\n>").join(quote.split('\n'))
+    bot.send_message(message.chat.id, '*Цитата/Мысль\ дня*:\n' + '>' + quote, parse_mode="MarkdownV2")
+
+@bot.message_handler(commands=['healthcheck'])
+def healthcheck(message):
+    bot.send_message(message.chat.id, "Я живой!")
+
+bot.infinity_polling()
---
+++ utils.py
@@ -0,0 +1,29 @@
+import json, os, requests
+
+from dotenv import load_dotenv
+
+load_dotenv()
+file_id = os.getenv("FILE_ID")
+
+def get_file_data():
+    download_url = f'https://drive.google.com/uc?export=download&id={file_id}'
+
+    response = requests.get(download_url)
+
+    response.raise_for_status()
+
+    return json.loads(response.content)
+
+def get_quotes():
+    data = get_file_data() # получить доступ к содержимому файла
+    quotes = []
+    for el in data['messages']:
+        if el['type'] == 'message':
+            if type(el['text']) is list:
+                quotes.append(el['text'][0]['text'])
+            else:
+                quotes.append(el['text'])
+    return quotes
+
+
+
