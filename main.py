95f7aff5d71993ce57dc13a86e58d86d2fe84a4b
Sakagam1 <platonov.ivan347@yandex.ru>
2024-08-03 13:07:48 +0300

    Final version, add ability to send quote and end program

--- main.py
+++ main.py
@@ -9,23 +9,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 token = os.getenv("TG_TOKEN")
-chat_id = int(os.getenv("CHAT_ID"))
+chat_bot_id = int(os.getenv("CHAT_BOT_ID"))
 user_id = os.getenv("USER_ID")
 
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['get_quote'])
-def get_quote(message):
+def get_quote():
     quotes = utils.get_quotes()
     num = secrets.SystemRandom().randint(0, len(quotes) - 1)
     quote = quotes[num]
     for ch in ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!']:
         quote = ("\\" + ch).join(quote.split(ch))
     quote = ("\n>").join(quote.split('\n'))
-    bot.send_message(message.chat.id, '*Цитата/Мысль\ дня*:\n' + '>' + quote, parse_mode="MarkdownV2")
+    bot.send_message(chat_bot_id, '*Цитата/Мысль\ дня*:\n' + '>' + quote, parse_mode="MarkdownV2")
 
 @bot.message_handler(commands=['healthcheck'])
 def healthcheck(message):
     bot.send_message(message.chat.id, "Я живой!")
 
-bot.infinity_polling()
+if __name__ == '__main__':
+    get_quote()
