0f5b6a644ef2fbf8c29c6fd144a5c86d94a0ff33
Sakagam1 <platonov.ivan347@yandex.ru>
2024-08-04 11:50:47 +0300

    Refactor code, add README.md, remove unnecessary secrets in workflow

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os, json, secrets
-import telebot
-
+import os
+import secrets
 import utils
 
+import telebot
 
 from dotenv import load_dotenv
 
@@ -14,21 +14,43 @@ user_id = os.getenv("USER_ID")
 
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(commands=['get_quote'])
+
+@bot.message_handler(commands=["get_quote"])
 def get_quote():
     quotes = utils.get_quotes()
     num = secrets.SystemRandom().randint(0, len(quotes) - 1)
     quote = quotes[num]
-    for ch in ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!']:
+    for ch in [
+        "_",
+        "*",
+        "[",
+        "]",
+        "(",
+        ")",
+        "~",
+        "`",
+        ">",
+        "#",
+        "+",
+        "-",
+        "=",
+        "|",
+        "{",
+        "}",
+        ".",
+        "!",
+    ]:
         quote = ("\\" + ch).join(quote.split(ch))
-    quote = ("\n>").join(quote.split('\n'))
-    bot.send_message(chat_bot_id, '*Цитата/Мысль\ дня*:\n' + '>' + quote, parse_mode="MarkdownV2")
+    quote = ("\n>").join(quote.split("\n"))
+    bot.send_message(
+        chat_bot_id, "*Цитата/Мысль\ дня*:\n" + ">" + quote, parse_mode="MarkdownV2"
+    )
 
-@bot.message_handler(commands=['healthcheck'])
+
+@bot.message_handler(commands=["healthcheck"])
 def healthcheck(message):
     bot.send_message(message.chat.id, "Я живой!")
 
-if __name__ == '__main__':
-    print("Script started")
+
+if __name__ == "__main__":
     get_quote()
-    print("OK")
--- utils.py
+++ utils.py
@@ -1,12 +1,15 @@
-import json, os, requests
+import json
+import os
+import requests
 
 from dotenv import load_dotenv
 
 load_dotenv()
 file_id = os.getenv("FILE_ID")
 
+
 def get_file_data():
-    download_url = f'https://drive.google.com/uc?export=download&id={file_id}'
+    download_url = f"https://drive.google.com/uc?export=download&id={file_id}"
 
     response = requests.get(download_url)
 
@@ -14,16 +17,14 @@ def get_file_data():
 
     return json.loads(response.content)
 
+
 def get_quotes():
-    data = get_file_data() # получить доступ к содержимому файла
+    data = get_file_data()  # получить доступ к содержимому файла
     quotes = []
-    for el in data['messages']:
-        if el['type'] == 'message':
-            if type(el['text']) is list:
-                quotes.append(el['text'][0]['text'])
+    for el in data["messages"]:
+        if el["type"] == "message":
+            if type(el["text"]) is list:
+                quotes.append(el["text"][0]["text"])
             else:
-                quotes.append(el['text'])
+                quotes.append(el["text"])
     return quotes
-
-
-
